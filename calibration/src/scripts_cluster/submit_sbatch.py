b6ec96b41a15d0b068effb875332e319f3c21669
benjaminboitrelle <benjamin.boitrelle@desy.de>
2019-10-15 14:14:25 +0200

    Start test of script for Maxwell
    First test for reading a dummy config file

--- calibration/src/scripts_cluster/submit_sbatch.py
+++ calibration/src/scripts_cluster/submit_sbatch.py
@@ -8,8 +8,9 @@ import multiprocessing
 import os
 import sys
 import time
+from collections import defaultdict
 
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+CURRENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 CALIBRATION_DIR = os.path.dirname(CURRENT_DIR)
 CONFIG_DIR = os.path.join(CALIBRATION_DIR, "conf")
 SRC_DIR = os.path.join(CALIBRATION_DIR, "src")
@@ -31,38 +32,18 @@ if SHARED_DIR not in sys.path:
 import utils  # noqa E402
 
 
-def get_arguments:
+def get_arguments():
     global CONFIG_DIR
 
-    parser = argparse.ArgumentParser(description="Calibration tools for P2M")
-    parser.add_argument("-i", "--input",
-                        dest="input",
+    parser = argparse.ArgumentParser(description="P2M job submission tools")
+    parser.add_argument("-e", "--email",
+                        dest="email",
                         type=str,
-                        help=("Path of input directory containing HDF5 files "
-                              "to analyse"))
-    parser.add_argument("-o", "--output",
-                        dest="output",
-                        type=str,
-                        help="Path of output directory for storing files")
-    parser.add_argument("-r", "--run",
-                        dest="run_id",
-                        type=str,
-                        help="Non-changing part of file name")
-    parser.add_argument("-m", "--method",
-                        dest="method",
-                        type=str,
-                        help="Method to use during the analysis: "
-                             "process_adccal_default, "
-                             "None")
+                        help="Email address for sending notification")
     parser.add_argument("-t", "--type",
                         dest="run_type",
                         type=str,
                         help="Run type: gather, process")
-
-    parser.add_argument("--n_cols",
-                        help="The number of columns to be used for splitting "
-                             "into subsets (to use all, set n_cols to None)")
-
     parser.add_argument("--config_file",
                         type=str,
                         default="default.yaml",
@@ -78,9 +59,52 @@ def get_arguments:
 
     return args
 
+def insert_args_into_config(args, config):
+
+    # general
+    if "general" not in config:
+#        print(config)
+        config = defaultdict(dict)
+
+    c_general = config["general"]
+
+    try:
+        c_general["run_type"] = args.run_type or c_general["run_type"]
+    except KeyError:
+        raise Exception("No run_type specified. Abort.")
+        sys.exit(1)
+
+    try:
+        c_general["email"] = args.email or c_general["email"]
+    except KeyError:
+        raise Exception("No run_id specified. Abort.")
+        sys.exit(1)
+
+
 class SubmitJobs(object):
     def __init__(self):
+        global CONF_DIR
+        global CALIBRATION_DIR
+        # Get command line arguments
+        args = get_arguments()
+
+        # Load user config file
+        config_usr = args.config_file
+
+#        yaml_usr = os.path.join(CONFIG_DIR, "{}".format(config_usr))
+        config = dict()
+        config = utils.load_config(config_usr)
+        insert_args_into_config(args, config)
+
+        print("Configuration:")
+        print(json.dumps(config_usr, sort_keys=True, indent=4))
+        try:
+            self.mail_address = config["general"]["email"]
+        except KeyError:
+            self.mail_address = None
+        print(self.mail_address)
 
 
+if __name__ == "__main__":
 
-
+    obj = SubmitJobs()
