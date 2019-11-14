acf600ee793a39d6c683c870eb38915c8b836b25
benjaminboitrelle <benjamin.boitrelle@desy.de>
2019-11-14 16:04:13 +0100

    Create non linear fitting method
    This method is aimed for applying non-linear fits on fine value and to 
    store these values for a future correction. There is at the moment only 
    a dumb structure.

---
+++ calibration/src/process/adccal/methods/process_non_linear_fitting.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Method for non-linear fitting of fine part of ADCs."""
+import __init__  # noqa F401
+from process_adccal_base import ProcessAdccalBase
+
+
+class Process(ProcessAdccalBase):
+    def _initiate(self):
+        pass
+
+    def get_length_crs_values(self, coarse, coarse_value):
+        pass
+
+    def _calulate(self):
+        pass
